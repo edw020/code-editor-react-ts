a778ce733bf623a46784903f544fa23c337764bf
edw020 <edw020@gmail.com>
2022-01-06 16:01:09 -0600

    Setting up state bundles for Code cells

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,6 +1,15 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
-import { UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellAfterAction, Direction } from '../actions';
+import {
+    UpdateCellAction,
+    DeleteCellAction,
+    MoveCellAction,
+    InsertCellAfterAction,
+    Direction,
+    Action
+} from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
     return {
@@ -36,5 +45,26 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
             id,
             type: cellType
         }
+    }
+};
+
+export const createBundle = (cellId: string, input: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BUNDLE_START,
+            payload: {
+                cellId,
+            }
+        });
+
+        const result = await bundle(input);
+
+        dispatch({
+            type: ActionType.BUNDLE_COMPLETE,
+            payload: {
+                cellId,
+                bundle: result
+            }
+        });
     }
 };
